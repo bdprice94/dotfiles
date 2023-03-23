453090407d731201376acc33a29017f3fadb795f
Benjamin Price <ben.price2013@gmail.com>
2023-03-22 19:48:35 -0500

    Add bootstrap script for setting up dev env on a new linux system

---
+++ bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Script for quickly setting up my vim developer configs on a new Linux system. Currently only known to work on Arch
+set -e
+
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+else
+    echo "Error: Could not find OS" >&2 
+    return -1
+fi
+if [[ $OS == "Arch Linux" ]]; then
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --noconfirm vim git github-cli zsh zsh-completions fzf bat nodejs python base-devel tmux
+else
+    sudo apt-get update
+    sudo apt-get -y install vim git gh nodejs zsh fzf bat python 
+fi
+mkdir ~/.vim
+git clone https://github.com/bdprice94/dotfiles.git
+ln dotfiles/vimrc ~/.vim/vimrc
+ln dotfiles/.p10k.zsh ~/.p10k.zsh
+ln dotfiles/.tmux.conf ~/.tmux.conf
+ln dotfiles/.zshrc ~/.zshrc
+# ln dotfiles/.vimspector.json ~/.vimspector.json
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if [[ $OS == "Arch Linux" ]]; then
+    cd ~
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg --noconfirm -si
+    
+    yay -Y --noconfirm --gendb
+    yay -Syu --noconfirm --devel
+    yay -Y --noconfirm --devel --save
+    yay -Syu --noconfirm 
+    yay -S --noconfirm zsh-theme-powerlevel10k-git zsh-vi-mode
+else
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+fi
+
+cd ~
+rm -rf yay
